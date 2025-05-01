ffafb6d9ed741d617014ff4dd75bcbd7cc084958
Mayank Gupta <mayankkumargupta03@gmail.com>
2025-05-01 18:17:04 +0530

    v1 backend complete

--- asset_onboarding/src/main/java/com/vsnt/asset_onboarding/config/KafkaConsumer.java
+++ asset_onboarding/src/main/java/com/vsnt/asset_onboarding/config/KafkaConsumer.java
@@ -1,14 +1,19 @@
 package com.vsnt.asset_onboarding.config;
 
 import com.vsnt.asset_onboarding.dtos.UpdateRequestDTO;
+import com.vsnt.asset_onboarding.services.AssetService;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 
 @Component
 public class KafkaConsumer {
+    private final AssetService assetService;
+    public KafkaConsumer(AssetService assetService) {
+        this.assetService = assetService;
+    }
     @KafkaListener(topics = "updates",groupId = "consumer-group-id")
     public void listen(Message<UpdateRequestDTO> message) {
-        System.out.println(message.getPayload());
+       assetService.updateAssetStatus(Long.parseLong(message.getPayload().getUploadId()),message.getPayload().getStatus());
     }
 }
--- asset_onboarding/src/main/java/com/vsnt/asset_onboarding/services/AssetService.java
+++ asset_onboarding/src/main/java/com/vsnt/asset_onboarding/services/AssetService.java
@@ -1,6 +1,7 @@
 package com.vsnt.asset_onboarding.services;
 
 import com.vsnt.asset_onboarding.entities.Upload;
+import com.vsnt.asset_onboarding.entities.enums.UploadStatus;
 import com.vsnt.asset_onboarding.repositories.UploadRepository;
 import org.springframework.stereotype.Service;
 
@@ -29,5 +30,16 @@ public class AssetService {
     }
     public List<Upload> getAllAssetsByUserId(String userId) {
         return uploadRepository.findAllByUserId(userId);
+    }
+    public void updateAssetStatus(long id, String status)
+    {
+        Upload upload = uploadRepository.findById(id);
+        if(upload==null){
+            System.out.println("Invalid update");
+            return;
+        }
+        upload.setUploadStatus(status.equals("SUCCESS")?UploadStatus.COMPLETED:UploadStatus.FAILED);
+
+
     }
 }
