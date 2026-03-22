8fea952a58fe07281dd7ddcb708695d12565aadf
Mayank Gupta <mayankkumargupta03@gmail.com>
2026-03-22 20:18:36 +0530

    generating access tokens and validating in DefaultDeliverySecurityConfig.java

--- asset_onboarding/src/main/java/com/vsnt/asset_onboarding/config/DefaultDeliverySecurityConfig.java
+++ asset_onboarding/src/main/java/com/vsnt/asset_onboarding/config/DefaultDeliverySecurityConfig.java
@@ -2,13 +2,19 @@ package com.vsnt.asset_onboarding.config;
 
 import com.vsnt.asset_onboarding.dtos.kvstore.segments.KVSegment;
 import com.vsnt.asset_onboarding.entities.Media;
+import com.vsnt.asset_onboarding.entities.MediaAccessToken;
 import com.vsnt.asset_onboarding.entities.enums.MediaType;
+import com.vsnt.asset_onboarding.repositories.MediaAccessTokenRepository;
 import com.vsnt.asset_onboarding.strategies.delivery.DeliverySecurityConfig;
+import com.vsnt.asset_onboarding.utils.JWTService;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+
+import java.sql.Timestamp;
+import java.time.Instant;
 
 @ConditionalOnProperty(
         name = "app.delivery.security.enabled",
@@ -17,9 +23,12 @@ import org.springframework.stereotype.Component;
 )
 @Component
 public class DefaultDeliverySecurityConfig implements DeliverySecurityConfig {
+    private final JWTService jwtService;
+    private final MediaAccessTokenRepository mediaAccessTokenRepository;
+    public DefaultDeliverySecurityConfig(JWTService jwtService, MediaAccessTokenRepository mediaAccessTokenRepository) {
 
-    public DefaultDeliverySecurityConfig() {
-
+        this.jwtService = jwtService;
+        this.mediaAccessTokenRepository = mediaAccessTokenRepository;
     }
 
     /*Default Delivery Security Config
@@ -60,11 +69,32 @@ public class DefaultDeliverySecurityConfig implements DeliverySecurityConfig {
     @Override
     public String[] generateTokens(String userId, String assetId) {
 
-        return new String[]{};
+        String access_token = jwtService.generateToken(userId ,assetId ,10*60*1000L);
+        MediaAccessToken token = new MediaAccessToken();
+        token.setUserId(userId);
+        token.setAssetId(assetId);
+        token.setRefreshToken(jwtService.generateToken(userId,assetId,24*60*60* 1000L));
+        token.setValidity(60*60* 1000L);
+        token = mediaAccessTokenRepository.save(token);
+        return new String[]{access_token,token.getRefreshToken()};
     }
 
     @Override
     public String refreshToken(HttpServletResponse httpServletResponse, String userId, String assetId, String token) {
-         return "";
+        MediaAccessToken savedToken = mediaAccessTokenRepository.findByRefreshToken(token).orElse(null);
+        if(savedToken == null ||savedToken.getCreatedAt().compareTo(Timestamp.from(Instant.now())) <= savedToken.getValidity()  )
+        {
+            throw new IllegalArgumentException("Invalid token");
+        }
+        if(jwtService.isTokenExpired(savedToken.getRefreshToken()))
+        {
+            throw new RuntimeException("Token is expired");
+        }
+        if(!savedToken.getUserId().equals(userId) || !savedToken.getAssetId().equals(assetId))
+        {
+            throw new IllegalArgumentException("Invalid token");
+        }
+
+        return jwtService.generateToken(userId ,assetId ,10*60*1000L);
     }
 }
